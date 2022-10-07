076862abe24350e33588f9e7380dd6e3f83121fc
ElChetoMDQ97 <braianher5@gmail.com>
2022-10-07 10:27:44 -0300

    v1.0.0A

---
+++ api/src/routes/index.js
@@ -0,0 +1,14 @@
+const { Router } = require('express');
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+
+
+const router = Router();
+
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+
+
+// https://restcountries.com/v3.1/all
+
+module.exports = router;
